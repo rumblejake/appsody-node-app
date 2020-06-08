3060fbf5e0d907aec94e91807025bedc7081df82
Jake <Jake Rumble>
2020-06-08 12:43:05 +0100

    another commit

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ app.get("/", (req, res) => {
   res.send("Hello from Appsody Demo! to Hello from Cloud Paks !!!.");
 });
 
+app.get("/jake", (req, res) => {
+  res.send("Hello Jake");
+});
+
 module.exports.app = app;
